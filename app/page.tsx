0eb1764e28918e3dffbf7fe507e2256c56789f3c
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-02-04 18:35:50 -0300

    Incluir nome na lista para identificação do projeto junto a publicação na Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,9 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Guilherme</li>
+          <li>Lacerda</li>
+          <li>Ribeiro</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
