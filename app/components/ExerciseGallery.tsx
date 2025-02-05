72ce1fb7fc1dcb4945ff084d5c96e93b3148fb87
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-02-05 09:00:41 -0300

    adição do react-swipeable para navegação entre os exercícios

--- app/components/ExerciseGallery.tsx
+++ app/components/ExerciseGallery.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
 import Exercise from "./Exercise";
 
 interface ExerciseData {
@@ -24,10 +25,8 @@ interface TreinoData {
 }
 
 const ExerciseGallery = () => {
-  const [treino, setTreino] = useState<TreinoData>({
-    data: "",
-    fichas: [],
-  });
+  const [treino, setTreino] = useState<TreinoData>({ data: "", fichas: [] });
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     fetch("/data/treino.json")
@@ -35,23 +34,54 @@ const ExerciseGallery = () => {
       .then((data) => setTreino(data));
   }, []);
 
+  const exercises = treino.fichas.flatMap((ficha) => ficha.exercises);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : exercises.length - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex < exercises.length - 1 ? prevIndex + 1 : 0));
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: handleNext,
+    onSwipedRight: handlePrev,
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true, // Permite arrastar com o mouse também
+  });
+
   return (
-    <div>
-      {treino.fichas.map((ficha) =>
-        ficha.exercises.map((exercise) => (
+    <div {...handlers} className="relative w-full max-w-lg mx-auto overflow-hidden">
+      {exercises.length > 0 && (
+        <div className="flex items-center justify-center">
           <Exercise
-            key={exercise.id}
-            id={exercise.id}
-            name={exercise.name}
-            image={`/media/images/${exercise.image}`}
-            series={exercise.series}
-            repetitions={exercise.repetitions}
-            rest={exercise.rest}
-            weight={exercise.weight}
-            video={`/media/videos/${exercise.video}`}
+            key={exercises[currentIndex].id}
+            id={exercises[currentIndex].id}
+            name={exercises[currentIndex].name}
+            image={`/media/images/${exercises[currentIndex].image}`}
+            series={exercises[currentIndex].series}
+            repetitions={exercises[currentIndex].repetitions}
+            rest={exercises[currentIndex].rest}
+            weight={exercises[currentIndex].weight}
+            video={`/media/videos/${exercises[currentIndex].video}`}
           />
-        ))
+        </div>
       )}
+
+      {/* Botões de navegação */}
+      <button
+        onClick={handlePrev}
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-500 bg-opacity-50 text-white p-2 rounded-full"
+      >
+        ◀
+      </button>
+      <button
+        onClick={handleNext}
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-500 bg-opacity-50 text-white p-2 rounded-full"
+      >
+        ▶
+      </button>
     </div>
   );
 };
