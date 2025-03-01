9e0a8e644a4b1478c1863ddbe15e7e21a460b918
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-01 20:01:37 -0300

    slider rotate and positioning of button

--- src/components/WorkoutExercises/index.jsx
+++ src/components/WorkoutExercises/index.jsx
@@ -4,13 +4,17 @@ import Plan from "../Plan";
 
 const WorkoutExercises = ({ exercises }) => {
   const [actualIdxSlide, setActualIdxSlide] = useState(0)
-  const prevSlide = () => setActualIdxSlide(actualIdxSlide - 1)
-  const nextSlide = () => setActualIdxSlide(actualIdxSlide + 1)
+  
+  // const lastIdxExercise = exercises.length - 1
+  // const prevSlide = () => setActualIdxSlide((prev) => prev === 0 ? lastIdxExercise : prev - 1)
+  // const nextSlide = () => setActualIdxSlide((prev) => prev === lastIdxExercise ? 0 : prev + 1)
+  const prevSlide = () => setActualIdxSlide((prev) => (prev - 1 + exercises.length) % exercises.length)
+  const nextSlide = () => setActualIdxSlide((prev) => (prev + 1) % exercises.length)
 
   return (
     <div className="slider">
-      <button onClick={prevSlide}>⬅️</button>
-      <button onClick={nextSlide}>➡️</button>
+      <button className="styled_button prev" onClick={prevSlide}></button>
+      <button className="styled_button next" onClick={nextSlide}></button>
       <ol className="slides" style={{ transform: `translateX(-${actualIdxSlide * 100}%)` }}>
         <Plan exercises={exercises} />
       </ol>
